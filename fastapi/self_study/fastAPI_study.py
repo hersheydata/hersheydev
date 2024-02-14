7c59bad3653c729e4da0f891dd89a951b1446b94
hersheythings <36656801+hersheythings@users.noreply.github.com>
2024-02-15 04:25:21 +0900

    Update fastAPI_study.py

--- fastapi/self_study/fastAPI_study.py
+++ fastapi/self_study/fastAPI_study.py
@@ -1,3 +1,17 @@
-def sample():
-  ls = []
-  return ls
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    description: str = None
+    price: float
+    tax: float = None
+
+app = FastAPI()
+
+@app.post('/items/')
+async def create_item(item: Item):
+    return item
+
+# 구조 1. pydantic을 사용해서 api(post)에서 보낼 데이터 모델 검증
+# 구조 2. 정의된 데이터 클래스 생성해주는 post api 개발
